f9e73695e22d8ceb6944349e10c31717f3bb9e70
madhulikaV <madhulika.vish@gmail.com>
2020-12-27 15:23:07 +0530

    phase 5 project adding

---
+++ src/test/java/com/ecom/CiCdDemoApp/CiCdDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.CiCdDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
